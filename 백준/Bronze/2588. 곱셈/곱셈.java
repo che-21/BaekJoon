4c62fcb4ea6babca81f011f3601a246a190aaedf
che-21 <105775827+che-21@users.noreply.github.com>
2023-08-12 18:37:26 +0900

    [Bronze III] Title: 곱셈, Time: 116 ms, Memory: 12808 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		int A, B;
+		Scanner sc = new Scanner(System.in);
+		A = sc.nextInt();
+		B = sc.nextInt();
+		System.out.println(A*(B%10));
+		System.out.println(A*((B%100)-(B%10))/10);
+		System.out.println(A*(B-(B%100))/100);
+		System.out.println(A*(B%10) + A*((B%100)-(B%10)) + A*(B-(B%100)));
+	}
+}
