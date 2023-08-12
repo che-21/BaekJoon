5237e22941397717dc405a9c9178c4da1696a01d
che-21 <105775827+che-21@users.noreply.github.com>
2023-08-12 18:45:37 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {	
+		String S;
+		
+		Scanner sc = new Scanner(System.in);
+		S = sc.next();
+		
+		String arr[] = S.split("");
+		System.out.println(arr.length);
+	}	
+}
